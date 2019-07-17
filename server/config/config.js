beff8ac896f41903dc57f63f5a26e71b2c73b5e6
Xabier Remirez <ekron77@gmail.com>
2019-07-16 20:56:31 -0700

    RestServer - Udemy: Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ekron:9wsrGo07VFzoR0JA@cluster0-krhak.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
