6c49bca87a10c039d3c55c9b0f2d21a03e47432f
Xabier Remirez <ekron77@gmail.com>
2019-07-18 14:59:07 -0700

    RestServer - Udemy: Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,27 @@
-// ==============
-// ====Puerto====
-// ==============
+// =================================
+// Puerto
+// =================================
 process.env.PORT = process.env.PORT || 3000;
 
-// ==============
-// ====Entorno===
-// ==============
+// =================================
+// Entorno
+// =================================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// ==============
-// Base de datos
-// ==============
+// =================================
+// Vencimiento del token
+// 60segs * 60mins * 24hrs * 30dias
+// =================================
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// =================================
+// SEED (Semilla)
+// =================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// =================================
+// Base de datos (Url)
+// =================================
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
---
+++ server/routes/login.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+app.post('/login', (req, res) => {
+    let body = req.body;
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                error: err
+            });
+        }
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                error: { message: 'El usuario/contraseña no es correcto' }
+            });
+        }
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                error: { message: 'El usuario/contraseña no es correcto' }
+            });
+        }
+        let token = jwt.sign({ usuario: usuarioDB }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token
+        });
+    });
+});
+
+module.exports = app;
